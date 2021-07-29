23a9fd2ad78a7f66673836af75fde1516b9482e5
Tanmay <tanmayvashistha.mail@gmail.com>
2021-07-29 17:35:46 +0530

    Removed extra / at link

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://lit-taiga-64278.herokuapp.com//register', {
+    fetch('https://lit-taiga-64278.herokuapp.com/register', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -18,7 +18,7 @@ class Signin extends React.Component {
   }
 
   onSubmitSignIn = () => {
-    fetch('https://lit-taiga-64278.herokuapp.com//signin', {
+    fetch('https://lit-taiga-64278.herokuapp.com/signin', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
