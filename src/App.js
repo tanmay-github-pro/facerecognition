b8eb0144e73342d9ce81e1b63153991fe003ab83
Tanmay <tanmayvashistha.mail@gmail.com>
2021-07-29 17:30:17 +0530

    Changed localhost to heroku provided url

--- src/App.js
+++ src/App.js
@@ -111,7 +111,7 @@ class App extends Component {
       .then(response => {
         console.log('hi', response)
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://lit-taiga-64278.herokuapp.com/image', {
             method: 'put',
             headers: { 'Content-Type': 'application/json' },
             body: JSON.stringify({
