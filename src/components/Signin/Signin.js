16324f8ef5de66e67d9aa592cb7853e732dc8af4
Tanmay <tanmayvashistha.mail@gmail.com>
2022-01-02 03:43:34 +0530

    Changed styling of signin componenet

--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -44,8 +44,9 @@ class Signin extends React.Component {
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
               <legend className="f1 fw6 ph0 mh0">Sign In</legend>
               <div className="mt3">
-                <label className="db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <label className="pa2 db fw6 lh-copy f6" htmlFor="email-address">Email</label>
                 <input
+                  style = {{ height: '3rem' }}
                   className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="email"
                   name="email-address"
@@ -54,8 +55,9 @@ class Signin extends React.Component {
                 />
               </div>
               <div className="mv3">
-                <label className="db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <label className="pa2 db fw6 lh-copy f6" htmlFor="password">Password</label>
                 <input
+                  style = {{ height: '3rem' }}
                   className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
                   type="password"
                   name="password"
@@ -66,6 +68,7 @@ class Signin extends React.Component {
             </fieldset>
             <div className="">
               <input
+                // style = {{ height: '3rem' }}
                 onClick={this.onSubmitSignIn}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
