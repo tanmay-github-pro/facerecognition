014b346360e2c1ec9bb90c45f17a20ab847b560d
Tanmay <tanmayvashistha.mail@gmail.com>
2022-02-01 19:17:21 +0530

    change appearance of component and accomodate for lower screen size using media queries

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -5,17 +5,17 @@ const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
     <div>
       <p className='f3'>
-        {'This Magic Brain will detect faces in your pictures. Git it a try.'}
+        {'This Magic Brain will detect faces in your pictures. Give it a try.'}
       </p>
-      <div className='center'>
-        <div className='form center pa4 br3 shadow-5'>
-          <input className='f4 pa2 w-70 center' type='tex' onChange={onInputChange}/>
+      {/* <div className='center'> */}
+        <div className='form center pa4 br3 shadow-5 custom-contain'>
+          <input className='f4 pa2 w-70 center' type='text' onChange={onInputChange}/>
           <button
-            className='w-30 grow f4 link ph3 pv2 dib white bg-light-purple'
+            className='w-25 grow f4 f4-m f4-l f7 link ph3 pv2 white bg-light-purple'
             onClick={onButtonSubmit}
           >Detect</button>
         </div>
-      </div>
+      {/* </div> */}
     </div>
   );
 }
