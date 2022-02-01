c619a3c60d35600ef5c62e6e7122102a92de5d55
Tanmay <tanmayvashistha.mail@gmail.com>
2022-02-01 20:18:36 +0530

    Add padding to p element in component

--- src/components/ImageLinkForm/ImageLinkForm.js
+++ src/components/ImageLinkForm/ImageLinkForm.js
@@ -3,7 +3,7 @@ import './ImageLinkForm.css';
 
 const ImageLinkForm = ({ onInputChange, onButtonSubmit }) => {
   return (
-    <div>
+    <div id='image_link_form_component'>
       <p className='f3'>
         {'This Magic Brain will detect faces in your pictures. Give it a try.'}
       </p>
