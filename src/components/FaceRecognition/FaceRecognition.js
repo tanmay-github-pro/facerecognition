5434124da0b600c2826420d12cff6161b5e45f10
Tanmay <tanmayvashistha.mail@gmail.com>
2022-02-01 19:16:41 +0530

    change style to accomodate bounding box because of change in margin

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,12 +3,15 @@ import './FaceRecognition.css';
 
 const FaceRecognition = ({ imageUrl, box }) => {
   return (
-    <div className='center ma'>
-      <div className='absolute mt2'>
-        <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto'/>
-        <div className='bounding-box' style={{top: box.topRow, right: box.rightCol, bottom: box.bottomRow, left: box.leftCol}}></div>
+    <>
+      <div style={{ position: 'relative' }} className='center ma'>
+        <div className='absolute mt2'>
+          <img id='inputimage' alt='' src={imageUrl} width='500px' heigh='auto' />
+          <div className='bounding-box' style={{ top: box.topRow+63.999, right: box.rightCol, bottom: box.bottomRow+63.999, left: box.leftCol }}></div>
+        </div>
       </div>
-    </div>
+      {/* <div className='absolute footer' style={{ top: '100rem', minHeight: '4rem' }}></div> */}
+    </>
   );
 }
 
