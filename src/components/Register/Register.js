e0cac6ea9feccfd3e803a699e4abcb506e6525e9
Tanmay <tanmayvashistha.mail@gmail.com>
2022-02-01 19:04:49 +0530

    add handle keypress to detect keypress enter to submit and automatic refactor code by VS Code

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -11,21 +11,21 @@ class Register extends React.Component {
   }
 
   onNameChange = (event) => {
-    this.setState({name: event.target.value})
+    this.setState({ name: event.target.value })
   }
 
   onEmailChange = (event) => {
-    this.setState({email: event.target.value})
+    this.setState({ email: event.target.value })
   }
 
   onPasswordChange = (event) => {
-    this.setState({password: event.target.value})
+    this.setState({ password: event.target.value })
   }
 
   onSubmitSignIn = () => {
     fetch('https://lit-taiga-64278.herokuapp.com/register', {
       method: 'post',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         email: this.state.email,
         password: this.state.password,
@@ -41,18 +41,34 @@ class Register extends React.Component {
       })
   }
 
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      console.log(`Enter key pressed!!`);
+      this.props.setLoading(true);
+      this.onSubmitSignIn();
+    }
+  }
+
   render() {
     return (
-      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
+      <article style={{ borderRadius: '1rem' }} className="br3 ba b--black-10 mv4 w-75 w-75-m w-25-l mw6 shadow-5 center">
+        <main
+          onKeyPress={this.handleKeyPress}
+          style={{
+            backgroundImage: 'linear-gradient(89deg, #ao86de 0%, #59a3de 100%)',
+            backdropFilter: 'blur(0.1rem)',
+            boxShadow: '1.5rem 1.35rem 1.5rem rgba(30,30,30,0.5)',
+            borderRadius: '1rem'
+          }}
+          className="pa4 w-100 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Register</legend>
+              <legend className="f1 f1-m f1-l f3 fw5 ph0 mh0">Register</legend>
               <div className="mt3">
-                <label className="pa2 db fw6 lh-copy f6" htmlFor="name">Name</label>
+                <label className="pa2 db fw6 lh-copy f5 f5-m f5-l f7" htmlFor="name">Name</label>
                 <input
-                style = {{ height: '3rem' }}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  style={{ height: '3rem' }}
+                  className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
                   type="text"
                   name="name"
                   id="name"
@@ -60,10 +76,10 @@ class Register extends React.Component {
                 />
               </div>
               <div className="mt3">
-                <label className="pa2 db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <label className="pa2 db fw6 lh-copy f5 f5-m f5-l f7" htmlFor="email-address">Email</label>
                 <input
-                style = {{ height: '3rem' }}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  style={{ height: '3rem' }}
+                  className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
                   type="email"
                   name="email-address"
                   id="email-address"
@@ -71,10 +87,10 @@ class Register extends React.Component {
                 />
               </div>
               <div className="mv3">
-                <label className="pa2 db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <label className="pa2 db fw6 lh-copy f5 f5-m f5-l f7" htmlFor="password">Password</label>
                 <input
-                style = {{ height: '3rem' }}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  style={{ height: '3rem' }}
+                  className="pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
                   type="password"
                   name="password"
                   id="password"
@@ -84,11 +100,26 @@ class Register extends React.Component {
             </fieldset>
             <div className="">
               <input
-                onClick={this.onSubmitSignIn}
+                onClick={() => {
+                  this.props.setLoading(true);
+                  this.onSubmitSignIn();
+                }}
                 className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
                 type="submit"
                 value="Register"
+                style={{
+                  transitionTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+                  borderRadius: '28px'
+                }}
               />
+              <div className='sign-in-form__or-divider'>
+                <span className='sign-in-form__or-span'>or</span>
+              </div>
+              <div className="lh-copy mt3">
+                <p>
+                  Already a Member? <span onClick={() => this.props.onRouteChange('signin')} className="link dim black pointer">Sign In</span>
+                </p>
+              </div>
             </div>
           </div>
         </main>
--- src/components/Signin/Signin.js
+++ src/components/Signin/Signin.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import './Signin.css';
 
 class Signin extends React.Component {
   constructor(props) {
@@ -10,17 +11,17 @@ class Signin extends React.Component {
   }
 
   onEmailChange = (event) => {
-    this.setState({signInEmail: event.target.value})
+    this.setState({ signInEmail: event.target.value })
   }
 
   onPasswordChange = (event) => {
-    this.setState({signInPassword: event.target.value})
+    this.setState({ signInPassword: event.target.value })
   }
 
   onSubmitSignIn = () => {
     fetch('https://lit-taiga-64278.herokuapp.com/signin', {
       method: 'post',
-      headers: {'Content-Type': 'application/json'},
+      headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({
         email: this.state.signInEmail,
         password: this.state.signInPassword
@@ -31,23 +32,43 @@ class Signin extends React.Component {
         if (user.id) {
           this.props.loadUser(user)
           this.props.onRouteChange('home');
+          setTimeout(() => { this.props.setLoading(false); }, 750)
+        }
+        else {
+          setTimeout(() => { this.props.setLoading(false); }, 2000)
         }
       })
+  }
+
+  handleKeyPress = (e) => {
+    if (e.key === 'Enter') {
+      console.log(`Enter key pressed!!`);
+      this.props.setLoading(true);
+      this.onSubmitSignIn();
+    }
   }
 
   render() {
     const { onRouteChange } = this.props;
     return (
-      <article className="br3 ba b--black-10 mv4 w-100 w-50-m w-25-l mw6 shadow-5 center">
-        <main className="pa4 black-80">
+      <article style={{ borderRadius: '1rem' }} className="br3 ba b--black-10 mv4 w-75 w-75-m w-25-l mw6 shadow-5 center">
+        <main
+          onKeyPress={this.handleKeyPress}
+          // style={{
+          //   backgroundImage: 'linear-gradient(89deg, #ao86de 0%, #59a3de 100%)',
+          //   backdropFilter: 'blur(0.1rem)',
+          //   boxShadow: '1.5rem 1.35rem 1.5rem rgba(30,30,30,0.5)',
+          //   borderRadius: '1rem'
+          // }}
+          className="pa4 w-100 black-80">
           <div className="measure">
             <fieldset id="sign_up" className="ba b--transparent ph0 mh0">
-              <legend className="f1 fw6 ph0 mh0">Sign In</legend>
+              <legend className="f1 f1-m f1-l f3 fw5 ph0 mh0">Sign In</legend>
               <div className="mt3">
-                <label className="pa2 db fw6 lh-copy f6" htmlFor="email-address">Email</label>
+                <label className="pa2 db fw6 lh-copy f5 f5-m f5-l f7" htmlFor="email-address">Email</label>
                 <input
-                  style = {{ height: '3rem' }}
-                  className="pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  // style={{ height: '3rem' }}
+                  className="text_input pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
                   type="email"
                   name="email-address"
                   id="email-address"
@@ -55,10 +76,10 @@ class Signin extends React.Component {
                 />
               </div>
               <div className="mv3">
-                <label className="pa2 db fw6 lh-copy f6" htmlFor="password">Password</label>
+                <label className="pa2 db fw6 lh-copy f5 f5-m f5-l f7" htmlFor="password">Password</label>
                 <input
-                  style = {{ height: '3rem' }}
-                  className="b pa2 input-reset ba bg-transparent hover-bg-black hover-white w-100"
+                  // style={{ height: '3rem' }}
+                  className="text_input b pa2 input-reset ba b--black bg-transparent hover-bg-black hover-white w-100"
                   type="password"
                   name="password"
                   id="password"
@@ -68,15 +89,25 @@ class Signin extends React.Component {
             </fieldset>
             <div className="">
               <input
-                // style = {{ height: '3rem' }}
-                onClick={this.onSubmitSignIn}
-                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 dib"
+                // style = {{ height: '2rem' }}
+                onClick={() => {
+                  this.props.setLoading(true);
+                  this.onSubmitSignIn();
+                }}
+                className="b ph3 pv2 input-reset ba b--black bg-transparent grow pointer f6 f6-m f6-l f7 dib"
                 type="submit"
                 value="Sign in"
+                style={{
+                  transitionTimingFunction: 'cubic-bezier(0, 0, 0.2, 1)',
+                  borderRadius: '28px'
+                }}
               />
             </div>
+            <div className='sign-in-form__or-divider'>
+              <span className='sign-in-form__or-span'>or</span>
+            </div>
             <div className="lh-copy mt3">
-              <p  onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
+              <p onClick={() => onRouteChange('register')} className="f6 link dim black db pointer">Register</p>
             </div>
           </div>
         </main>
