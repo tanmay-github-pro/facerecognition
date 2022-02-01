35b95e6b5d953c896f7a41448a2f81652e243f0b
Tanmay <tanmayvashistha.mail@gmail.com>
2022-02-01 19:14:57 +0530

    Change styles and placement of logo component

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -5,10 +5,10 @@ import './Logo.css';
 
 const Logo = () => {
   return (
-    <div className='ma4 mt0'>
-      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: 150, width: 150 }} >
+    <div style={{/*marginRight: '20rem'*/}} className='ma4 mt0'>
+      <Tilt className="Tilt br2 shadow-2" options={{ max : 55 }} style={{ height: '5rem', width: 'auto' }} >
         <div className="Tilt-inner pa3">
-          <img style={{paddingTop: '5px'}} alt='logo' src={brain}/>
+          <img style={{display: 'inline', padding: '4px', width: 'auto', height: '80%'}} alt='logo' src={brain}/>
         </div>
       </Tilt>
     </div>
