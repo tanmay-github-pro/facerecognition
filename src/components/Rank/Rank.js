4a248ea8123e0938dae4568d4f85f35ea76e65dd
Tanmay <tanmayvashistha.mail@gmail.com>
2022-02-01 20:19:25 +0530

    Add id to rank component div to apply styles that change order of flex item to 3

--- src/components/Rank/Rank.js
+++ src/components/Rank/Rank.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Rank.css';
 
 const Rank = ({ name, entries }) => {
   return (
-    <div>
+    <div id='rank-component'>
       <div className='white f3'>
         {`${name}, your current entry count is...`}
       </div>
