d0ff96cf6c88b83ab099a9c6489fa447e3de8750
Tanmay <tanmayvashistha.mail@gmail.com>
2022-02-01 20:19:56 +0530

    change size of h1 in component on a small screen

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -11,7 +11,7 @@ const Navigation = ({ onRouteChange, isSignedIn }) => {
     } else {
       return (
         <nav style={{display: 'flex', flexWrap: 'wrap', justifyContent: 'center', alignItems: 'center', padding: '1.5rem 0'}}>
-          <h1 className='f1-m f1-l f3 fw5 ph0 mh0 center' style={{ /*marginLeft: '36vw', /*position: 'absolute', top: '20%', left: '50%', transform: 'translate(-50%, -50%)', /*fontSize: '4rem'*/}}>Face Recognition</h1>
+          <h1 className='f1-m f1-l f2 fw5 ph4 mh0 center' style={{ /*marginLeft: '36vw', /*position: 'absolute', top: '20%', left: '50%', transform: 'translate(-50%, -50%)', /*fontSize: '4rem'*/}}>Face Recognition</h1>
           {/* <p onClick={() => onRouteChange('signin')} className='f3 link dim black pa4 pa3-m pa3-l pt5 pt3-m pt3-l pointer'>Sign In</p> */}
           {/* <p onClick={() => onRouteChange('register')} className='f3 link dim black pa4 pa3-m pa3-l pt5 pt3-m pt3-l pointer'>Register</p> */}
           {/* <p className='f3 link dim black pa3 pointer'></p> */}
